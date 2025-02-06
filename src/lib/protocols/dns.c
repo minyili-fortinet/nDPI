467f2eb6d866fbcff9516d7062815be1fff1a700
Sebastian Gottschall <s.gottschall@dd-wrt.com>
2025-02-06 16:42:59 +0700

    fix 64 integer division on 32 bit host systems
    on 32 bit systems like mips32 or armv7, gcc will generate unresolved
    symbols like __udivdi3. which is not available in the kernel
    
    WARNING: "__udivdi3" [/home/seg/DEV/pb42/src/router/ndpi-netfilter/ndpi-netfilter/src/xt_ndpi.ko] undefined!
    
    Signed-off-by: Sebastian Gottschall <s.gottschall@dd-wrt.com>

--- src/lib/protocols/dns.c
+++ src/lib/protocols/dns.c
@@ -277,6 +277,7 @@ static int search_valid_dns(struct ndpi_detection_module_struct *ndpi_struct,
 			    u_int payload_offset, u_int8_t *is_query,
 			    u_int8_t ignore_checks) {
   struct ndpi_packet_struct *packet = ndpi_get_packet_struct(ndpi_struct);
+  u_int64_t time;
   u_int x = payload_offset;
 
   memcpy(dns_header, (struct ndpi_dns_packet_header*)&packet->payload[x],
@@ -484,13 +485,19 @@ static int search_valid_dns(struct ndpi_detection_module_struct *ndpi_struct,
 		    flow->protos.dns.is_rsp_addr_ipv6[flow->protos.dns.num_rsp_addr] = (data_len == 16) ? 1 : 0;
 		    flow->protos.dns.rsp_addr_ttl[flow->protos.dns.num_rsp_addr] = ttl;
 
-		    if(ndpi_struct->cfg.address_cache_size)
+		    if(ndpi_struct->cfg.address_cache_size) {
+		    #if defined(__KERNEL__) && !defined(CONFIG_64BIT)
+		      time = packet->current_time_ms;
+		      do_div(time, 1000);
+		    #else
+		      time = packet->current_time_ms / 1000;
+		    #endif
 		      ndpi_cache_address(ndpi_struct,
 				         flow->protos.dns.rsp_addr[flow->protos.dns.num_rsp_addr],
 				         flow->host_server_name,
-				         packet->current_time_ms/1000,
+				         time,
 				         flow->protos.dns.rsp_addr_ttl[flow->protos.dns.num_rsp_addr]);
-
+		      }
 		    ++flow->protos.dns.num_rsp_addr;
 		  }
 
